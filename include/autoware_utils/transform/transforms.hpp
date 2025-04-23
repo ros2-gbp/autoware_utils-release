46460b37ecdc6ba708a61bec7a159a69204ae2e7
Yutaka Kondo <yutaka.kondo@youtalk.jp>
2025-04-23 16:56:02 +0900

    Rebase from 'debian/rolling/autoware_utils'

--- include/autoware_utils/transform/transforms.hpp
+++ include/autoware_utils/transform/transforms.hpp
@@ -12,16 +12,16 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-#ifndef AUTOWARE_UTILS__ROS__UUID_HELPER_HPP_
-#define AUTOWARE_UTILS__ROS__UUID_HELPER_HPP_
+#ifndef AUTOWARE_UTILS__TRANSFORM__TRANSFORMS_HPP_
+#define AUTOWARE_UTILS__TRANSFORM__TRANSFORMS_HPP_
 
 // NOLINTBEGIN(build/namespaces, whitespace/line_length)
 // clang-format off
 
-#include <autoware_utils_uuid/uuid_helper.hpp>
-namespace autoware_utils { using namespace autoware_utils_uuid; }
+#include <autoware_utils_pcl/transforms.hpp>
+namespace autoware_utils { using namespace autoware_utils_pcl; }
 
 // clang-format on
 // NOLINTEND
 
-#endif  // AUTOWARE_UTILS__ROS__UUID_HELPER_HPP_
+#endif  // AUTOWARE_UTILS__TRANSFORM__TRANSFORMS_HPP_
