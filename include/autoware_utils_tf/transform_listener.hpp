ce5a6626558e7bf533d54345fb8d5f8f6eec43d9
mitsudome-r <ryohsuke.mitsudome@tier4.jp>
2026-07-31 18:50:26 +0900

    Rebase from 'release/humble/autoware_utils_tf'

--- include/autoware_utils_tf/transform_listener.hpp
+++ include/autoware_utils_tf/transform_listener.hpp
@@ -17,27 +17,29 @@
 
 #include <rclcpp/rclcpp.hpp>
 #include <tf2_ros/buffer.hpp>
-#include <tf2_ros/create_timer_ros.hpp>
 #include <tf2_ros/transform_listener.hpp>
 
 #include <geometry_msgs/msg/transform_stamped.hpp>
 
 #include <memory>
 #include <string>
+#include <type_traits>
 
 namespace autoware_utils_tf
 {
-class TransformListener
+template <class NodeT, class BufferT, class ListenerT>
+class TransformListenerT
 {
 public:
-  explicit TransformListener(rclcpp::Node * node)
-  : clock_(node->get_clock()), logger_(node->get_logger())
+  explicit TransformListenerT(NodeT * node) : clock_(node->get_clock()), logger_(node->get_logger())
   {
-    tf_buffer_ = std::make_shared<tf2_ros::Buffer>(clock_);
-    auto timer_interface = std::make_shared<tf2_ros::CreateTimerROS>(
-      node->get_node_base_interface(), node->get_node_timers_interface());
-    tf_buffer_->setCreateTimerInterface(timer_interface);
-    tf_listener_ = std::make_shared<tf2_ros::TransformListener>(*tf_buffer_);
+    tf_buffer_ = std::make_shared<BufferT>(clock_);
+    // The two buffer backends take different TransformListener constructors.
+    if constexpr (std::is_same_v<BufferT, tf2_ros::Buffer>) {
+      tf_listener_ = std::make_shared<ListenerT>(*tf_buffer_);
+    } else {
+      tf_listener_ = std::make_shared<ListenerT>(*tf_buffer_, *node);
+    }
   }
 
   geometry_msgs::msg::TransformStamped::ConstSharedPtr get_latest_transform(
@@ -78,9 +80,12 @@ public:
 private:
   rclcpp::Clock::SharedPtr clock_;
   rclcpp::Logger logger_;
-  std::shared_ptr<tf2_ros::Buffer> tf_buffer_;
-  std::shared_ptr<tf2_ros::TransformListener> tf_listener_;
+  std::shared_ptr<BufferT> tf_buffer_;
+  std::shared_ptr<ListenerT> tf_listener_;
 };
+
+using TransformListener =
+  TransformListenerT<rclcpp::Node, tf2_ros::Buffer, tf2_ros::TransformListener>;
 }  // namespace autoware_utils_tf
 
 #endif  // AUTOWARE_UTILS_TF__TRANSFORM_LISTENER_HPP_
